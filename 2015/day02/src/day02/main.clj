12c4a906e484aa62fee6dc9ce4fb5d5d1d16dd4d
Anatolij Grigorjev <grigorjev.anatolij@gmail.com>
2021-01-27 15:13:57 +0200

    day02 1* solution

--- 2015/day02/src/day02/main.clj
+++ 2015/day02/src/day02/main.clj
@@ -1,4 +1,30 @@
-(ns day02.main)
+(ns day02.main
+  (:require [clojure.java.io :as io])
+  (:require [clojure.string :as str]))
+
+(defn dims2sides [dims]
+  [(* (:w dims) (:l dims)) (* (:w dims) (:h dims)) (* (:h dims) (:l dims))])
+
+(defn surface-area [dims]
+  (* 2 (apply + (dims2sides dims))))
+
+(defn extra-slack [dims]
+  (apply min (dims2sides dims)))
+
+(defn parse-dim [text-line]
+  (zipmap [:l :w :h] (map #(Integer/parseInt %) (str/split text-line #"x" 3))))
+
+(defn read-input [path] 
+  (with-open [reader (io/reader path)]
+    (doall (map parse-dim (line-seq reader)))))
+
+(defn total-box-area [dims]
+  (+ (surface-area dims) (extra-slack dims)))
+
+(defn total-needed-feet [dims-list]
+  (reduce + (map total-box-area dims-list)))
 
 (defn -main [& args]
-  (println "Initializing day02"))
+  (let [path (first args)]
+    (println "Reading " path "...")
+    (println "Needed feet: " (total-needed-feet (read-input path)))))
