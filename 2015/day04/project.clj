43675675bcd6428bc1b97ca316b69c0a83dfd920
Anatolij Grigorjev <grigorjev.anatolij@gmail.com>
2021-01-29 15:51:34 +0200

    add empty day04 project

---
+++ 2015/day04/project.clj
@@ -0,0 +1,3 @@
+(defproject day04 "HEAD"
+  :dependencies [[org.clojure/clojure "1.9.0"]]
+  :main day04.main)
---
+++ 2015/day04/src/day04/main.clj
@@ -0,0 +1,13 @@
+(ns day04.main)
+
+(def input-key "yzbqklnj")
+
+
+
+
+
+(defn do-part-one [input]
+  (println "input is " input))
+
+(defn -main [& args]
+  (do-part-one input-key))
