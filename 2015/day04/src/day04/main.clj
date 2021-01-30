7eb1ffca11a4a70d674c9abe9d35cef324a45edc
Anatolij Grigorjev <grigorjev.anatolij@gmail.com>
2021-01-30 10:18:48 +0200

    add day04 2* logic

--- 2015/day04/src/day04/main.clj
+++ 2015/day04/src/day04/main.clj
@@ -1,13 +1,45 @@
 (ns day04.main)
 
+(import java.security.MessageDigest)
+
 (def input-key "yzbqklnj")
 
+; function taken from https://gist.github.com/jizhang/4325757
+(defn md5-bytes[s]
+  (->> s
+       .getBytes
+       (.digest (MessageDigest/getInstance "MD5"))))
 
+(defn md5-05? [bytes]
+  (and 
+   (zero? (first bytes))
+   (zero? (second bytes))
+   (<= 0 (nth bytes 2) 15)))
 
+(defn md5-06? [bytes]
+  (every? zero? (take 3 bytes)))
 
+(defn form-input [num]
+  (str input-key num))
 
-(defn do-part-one [input]
-  (println "input is " input))
+(defn find-first [f col]
+  (first (drop-while (complement f) col)))
+
+(defn key-inputs []
+  (map form-input (range)))
+
+(defn num-hash-pairs []
+  (map #(vector % (md5-bytes %)) (key-inputs)))
+
+(defn find-numbers [md5-pred]
+  (find-first #(md5-pred (second %)) (num-hash-pairs)))
+
+(defn do-part-one []
+  (println "lowest num with hash 00000....: " (find-numbers md5-05?)))
+
+(defn do-part-two []
+  (println "lowest num with hash 000000...: " (find-numbers md5-06?)))
 
 (defn -main [& args]
-  (do-part-one input-key))
+  (do-part-one)
+  (do-part-two))
