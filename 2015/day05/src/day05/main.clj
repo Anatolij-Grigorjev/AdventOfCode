d92179c907a5229eb05f921eec04b6978a3dd034
Anatolij Grigorjev <grigorjev.anatolij@gmail.com>
2021-01-31 11:04:23 +0200

    add day05 2015 1* logic

--- 2015/day05/src/day05/main.clj
+++ 2015/day05/src/day05/main.clj
@@ -1,4 +1,46 @@
-(ns day05.main)
+(ns day05.main
+  (:require [clojure.java.io :as io])
+  (:require [clojure.string :as str]))
+
+(def naughty-strings ["ab" "cd" "pq" "xy"])
+(def vowels "aeiou")
+
+
+(defn excludes-all? [word, substrings]
+  (empty? (filter #(str/includes? word %) substrings)))
+
+(defn has-n-vowels? [word n vowels]
+  (>= (count (filter (into #{} vowels) word)) n))
+
+(defn has-double-letter? [word]
+  (loop [letters word]
+    (let [letter1 (first letters)
+          letter2 (second letters)]
+      (if (nil? letter2)
+        false
+        (if (= letter1 letter2)
+          true
+          (recur (rest letters)))))))
+
+(defn is-nice? [word]
+  (and
+   (excludes-all? word naughty-strings)
+   (has-n-vowels? word 3 vowels)
+   (has-double-letter? word)))
+
+
+(defn do-part-one [words-list]
+  (count (filter is-nice? words-list)))
+
+
+(defn read-input [path]
+  (with-open [reader (io/reader path)]
+    (doall (map identity (line-seq reader)))))
+
 
 (defn -main [& args]
-  (println "Initializing day05"))
+  (let [path (first args)
+        words-list (read-input path)]
+    (println "Reading " path "...")
+    (println "Num words total: " (count words-list))
+    (println "Num nice words: " (do-part-one words-list))))
