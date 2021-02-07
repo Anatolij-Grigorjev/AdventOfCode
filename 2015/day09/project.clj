dac1f7deee08961a35acf553e8da117082b5e501
Anatolij Grigorjev <grigorjev.anatolij@gmail.com>
2021-02-07 11:19:30 +0200

    partial work on traversal graph for day09 1*

--- 2015/day09/project.clj
+++ 2015/day09/project.clj
@@ -1,5 +1,6 @@
 (defproject day09 "HEAD"
   :dependencies [
                  [org.clojure/clojure "1.9.0"]
+                 [ubergraph "0.8.2"]
                  [org.tiem625/aoc-commons "0.01"]]
   :main day09.main)
--- 2015/day09/src/day09/main.clj
+++ 2015/day09/src/day09/main.clj
@@ -1,4 +1,30 @@
-(ns day09.main)
+(ns day09.main
+  (:require [aoc-commons.core :refer :all])
+  (:require [clojure.string :as str])
+  (:require [ubergraph.core :as ug])
+  (:require [ubergraph.alg :as galg]))
+
+
+(defn parse-distances [line]
+  (let [city-from-and-rest (str/split line #" to " 2)
+        city-from (first city-from-and-rest)
+        city-to-and-distance (str/split (second city-from-and-rest) #" = " 2)
+        city-to (first city-to-and-distance)]
+    [city-from city-to (parse-int (second city-to-and-distance))]))
+
 
 (defn -main [& args]
-  (println "Initializing day09"))
+  (let [path (first args)
+        input-graph (apply ug/graph (read-input-lines parse-distances path))]
+    (println (ug/pprint input-graph))
+    (let [graph-paths (for [start-city (ug/nodes input-graph)
+                            end-city (ug/nodes input-graph)
+                            :when (not= start-city end-city)]
+                        (galg/shortest-path input-graph start-city end-city :weight))
+          full-paths (filter #(= (count (ug/nodes input-graph)) (count (galg/nodes-in-path %))) graph-paths)
+          cost-paths (map (fn [path]
+                            {:cost (galg/cost-of-path path)
+                             :edges (count (galg/edges-in-path path))
+                             :from (galg/start-of-path path)
+                             :to (galg/end-of-path path)}) full-paths)]
+      (doall (map println cost-paths)))))
