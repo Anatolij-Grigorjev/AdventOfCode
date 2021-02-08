ca09b61fcc49cf3ead5cdf7412cfbb1296df141b
Anatolij Grigorjev <grigorjev.anatolij@gmail.com>
2021-02-08 15:56:28 +0200

    day09 1* solution

--- 2015/day09/src/day09/main.clj
+++ 2015/day09/src/day09/main.clj
@@ -1,8 +1,7 @@
 (ns day09.main
   (:require [aoc-commons.core :refer :all])
   (:require [clojure.string :as str])
-  (:require [ubergraph.core :as ug])
-  (:require [ubergraph.alg :as galg]))
+  (:require [ubergraph.core :as ug]))
 
 
 (defn parse-distances [line]
@@ -13,18 +12,38 @@
     [city-from city-to (parse-int (second city-to-and-distance))]))
 
 
+(defn node-in-subset [nodes n]
+  (some #{n} nodes))
+
+(defn edge->str [g edge]
+  (str "-" (ug/weight g edge) "->" (ug/dest edge)))
+
+
+(defn shortest-edges [g start]
+  (loop [remaining-nodes (ug/nodes g)
+         start-node start
+         path-edges []]
+    (let [node-edges (ug/find-edges g {:src start-node})
+          forward-edges (filter #((partial node-in-subset remaining-nodes) (ug/dest %)) node-edges)
+          shortest-edge (first (reverse (sort-by #(ug/weight g %) forward-edges)))]
+      (if (empty? forward-edges)
+        path-edges
+        (do
+          (println "From: " start-node)
+          (doall (map #(println "\t" (edge->str g %)) forward-edges))
+          (println "Picked: " (edge->str g shortest-edge))
+          (recur (remove #{start-node (ug/dest shortest-edge)} remaining-nodes)
+                 (ug/dest shortest-edge)
+                 (conj path-edges shortest-edge)))))))
+
+
 (defn -main [& args]
   (let [path (first args)
         input-graph (apply ug/graph (read-input-lines parse-distances path))]
     (println (ug/pprint input-graph))
-    (let [graph-paths (for [start-city (ug/nodes input-graph)
-                            end-city (ug/nodes input-graph)
-                            :when (not= start-city end-city)]
-                        (galg/shortest-path input-graph start-city end-city :weight))
-          full-paths (filter #(= (count (ug/nodes input-graph)) (count (galg/nodes-in-path %))) graph-paths)
-          cost-paths (map (fn [path]
-                            {:cost (galg/cost-of-path path)
-                             :edges (count (galg/edges-in-path path))
-                             :from (galg/start-of-path path)
-                             :to (galg/end-of-path path)}) full-paths)]
-      (doall (map println cost-paths)))))
+    (let [shortest-paths-map (map #(vector % (shortest-edges input-graph %)) (ug/nodes input-graph))]
+      (doall (map (fn [[path-start shortest-path]]
+                    (println "shortest path from: " path-start (map (partial edge->str input-graph) shortest-path))
+                    (println "total shortest path: " (reduce + (map #(ug/weight input-graph %) shortest-path)))
+                    (println))
+                  shortest-paths-map)))))
