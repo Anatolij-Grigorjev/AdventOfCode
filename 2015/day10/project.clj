b8565a48e8d44de3e7b158387f12147d91c47de1
Anatolij Grigorjev <grigorjev.anatolij@gmail.com>
2021-02-08 16:51:01 +0200

    empty day10

---
+++ 2015/day10/project.clj
@@ -0,0 +1,3 @@
+(defproject day10 "HEAD"
+  :dependencies [[org.clojure/clojure "1.9.0"]]
+  :main day10.main)
---
+++ 2015/day10/src/day10/main.clj
@@ -0,0 +1,4 @@
+(ns day10.main)
+
+(defn -main [& args]
+  (println "Initializing day10"))
