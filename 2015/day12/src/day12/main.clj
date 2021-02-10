0d26970298366745436656d096a4f68b50cbb352
Anatolij Grigorjev <grigorjev.anatolij@gmail.com>
2021-02-10 15:58:12 +0200

    day12 1* solution

--- 2015/day12/src/day12/main.clj
+++ 2015/day12/src/day12/main.clj
@@ -1,4 +1,22 @@
-(ns day12.main)
+(ns day12.main
+  (:require [aoc-commons.core :refer :all])
+  (:require [clojure.data.json :as json]))
+
+(defn sum [col]
+  (reduce + col))
+
+(defn text-numers [text]
+  (map parse-int (re-seq #"-?\d+" text)))
+
+
+
+
+
+
 
 (defn -main [& args]
-  (println "Initializing day12"))
+  (let [path (first args)
+        json-string (slurp path)]
+    (println "read at path" path)
+    (println "JSON size:" (count json-string))
+    (println "All numbers sum in JSON:" (sum (text-numers json-string)))))
