fa1ef59082d53c95c646e6b93ded9709757264b6
Anatolij Grigorjev <grigorjev.anatolij@gmail.com>
2021-02-10 15:58:20 +0200

    day12 1* solution

--- 2015/day12/project.clj
+++ 2015/day12/project.clj
@@ -1,3 +1,5 @@
 (defproject day12 "HEAD"
-  :dependencies [[org.clojure/clojure "1.9.0"]]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.tiem625/aoc-commons "0.01"]
+                 [org.clojure/data.json "1.0.0"]]
   :main day12.main)
