546f5e4e1d52c8fcecf9e00a7db8aafa703e7189
Anatolij Grigorjev <grigorjev.anatolij@gmail.com>
2021-02-12 16:29:40 +0200

    empty day15

---
+++ 2015/day15/project.clj
@@ -0,0 +1,4 @@
+(defproject day15 "HEAD"
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.tiem625/aoc-commons "0.01"]]
+  :main day15.main)
---
+++ 2015/day15/src/day15/main.clj
@@ -0,0 +1,5 @@
+(ns day15.main
+  (:require [aoc-commons.core :refer :all]))
+
+(defn -main [& args]
+  (println "Initializing day15"))
