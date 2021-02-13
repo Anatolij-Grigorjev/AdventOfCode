c8d932d5c1cef3f69d9190df48716b32e412fc96
Anatolij Grigorjev <grigorjev.anatolij@gmail.com>
2021-02-13 11:46:30 +0200

    day15 solution copy from /u/derekslager

--- 2015/day15/project.clj
+++ 2015/day15/project.clj
@@ -1,4 +1,4 @@
 (defproject day15 "HEAD"
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.tiem625/aoc-commons "0.01"]]
+                 [org.clojure/math.combinatorics "0.1.6"]]
   :main day15.main)
--- 2015/day15/src/day15/main.clj
+++ 2015/day15/src/day15/main.clj
@@ -1,5 +1,32 @@
 (ns day15.main
-  (:require [aoc-commons.core :refer :all]))
+  (:require [clojure.math.combinatorics :refer [selections]]))
+
+; solution taken from https://github.com/derekslager
+
+(defn parse-input [resource]
+  (into [] (for [[_ ingredient props] (re-seq #"(\w+): (.*)" resource)]
+             [ingredient (into {} (for [[_ k v] (re-seq #"(\w+) (-?\d+)" props)]
+                                    [(keyword k) (Integer/parseInt v)]))])))
+
+(defn score-key [stats recipe key]
+  (max 0 (reduce + (map * recipe (for [[_ stat] stats] (key stat))))))
+
+(defn score [stats recipe]
+  (apply *
+         (for [key [:capacity :durability :flavor :texture]
+               :let [score (score-key stats recipe key)]
+               :while (not= 0 score)]
+           score)))
 
 (defn -main [& args]
-  (println "Initializing day15"))
+  (let [resource (slurp (first args))
+        stats (parse-input resource)
+        ingredients (set (map first stats))]
+    ;; part 1
+    (println (apply max
+           (for [recipe (selections (range 1 97) (count ingredients))
+                 :when (= 100 (reduce + recipe))
+                 ;; part 2
+                 ;:when (= 500 (score-key stats recipe :calories))
+                 ]
+             (score stats recipe))))))
