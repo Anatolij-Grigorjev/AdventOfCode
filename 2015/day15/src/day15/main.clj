768fd2bda1ce5d295ef6f5e08b4fa45e435c689a
Anatolij Grigorjev <grigorjev.anatolij@gmail.com>
2021-02-14 10:01:36 +0200

    adjusted for current input, day15 2* solution from derekslager

--- 2015/day15/src/day15/main.clj
+++ 2015/day15/src/day15/main.clj
@@ -1,7 +1,7 @@
 (ns day15.main
   (:require [clojure.math.combinatorics :refer [selections]]))
 
-; solution taken from https://github.com/derekslager
+; solution based on https://github.com/derekslager
 
 (defn parse-input [resource]
   (into [] (for [[_ ingredient props] (re-seq #"(\w+): (.*)" resource)]
@@ -14,8 +14,9 @@
 (defn score [stats recipe]
   (apply *
          (for [key [:capacity :durability :flavor :texture]
-               :let [score (score-key stats recipe key)]
-               :while (not= 0 score)]
+               :let [score (score-key stats recipe key)]]
+           ;; removed :while not 0 shortcut 
+           ;; it creates false result when only :texture score is 0
            score)))
 
 (defn -main [& args]
@@ -23,10 +24,12 @@
         stats (parse-input resource)
         ingredients (set (map first stats))]
     ;; part 1
-    (println (apply max
+    (doall (map println stats))
+    (println)
+    (let [best-cookie (apply max-key second
            (for [recipe (selections (range 1 97) (count ingredients))
                  :when (= 100 (reduce + recipe))
                  ;; part 2
-                 ;:when (= 500 (score-key stats recipe :calories))
-                 ]
-             (score stats recipe))))))
+                 :when (= 500 (score-key stats recipe :calories))]
+             [recipe (score stats recipe)]))]
+      (println "Best cookie balance:" (zipmap (map first stats) (first best-cookie)) "score:" (second best-cookie)))))
