5c0cd617b130c7df22916836fb32c5082e14c7fa
Anatolij Grigorjev <grigorjev.anatolij@gmail.com>
2021-02-15 11:13:55 +0200

    day 17 2* solution

--- 2015/day17/project.clj
+++ 2015/day17/project.clj
@@ -1,4 +1,5 @@
 (defproject day17 "HEAD"
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.tiem625/aoc-commons "0.01"]]
+                 [org.tiem625/aoc-commons "0.01"]
+                 [org.clojure/math.combinatorics "0.1.6"]]
   :main day17.main)
--- 2015/day17/src/day17/main.clj
+++ 2015/day17/src/day17/main.clj
@@ -1,10 +1,25 @@
 (ns day17.main
-  (:require [aoc-commons.core :as aoc]))
+  (:require [aoc-commons.core :as aoc])
+  (:require [clojure.math.combinatorics :refer [subsets permutations]]))
 
-(defn parse-bucket [line]
-  ((comp first first) (re-seq #"(\d+)" line)))
+(defn parse-bucket-capacity [line]
+  ((comp aoc/parse-int first first) (re-seq #"(\d+)" line)))
+
+(defn parse-buckets [path]
+  (let [capacities (aoc/read-input-lines parse-bucket-capacity path)]
+    (zipmap (map (partial str "bucket") (range (count capacities))) capacities)))
+
 
 (defn -main [& args]
   (let [path (first args)
-        buckets (aoc/read-input-lines parse-bucket path)]
-    (println buckets)))
+        buckets (apply vector (parse-buckets path))
+        sum 150]
+    (println (count buckets) "buckets ->" sum "liters")
+    (let [fit-buckets (for [buckets-selection (subsets buckets)
+                            :let [buckets-contain (reduce + 0 (map second buckets-selection))]
+                            :when (= sum buckets-contain)]
+                        buckets-selection)]
+      (println "fit combinations:" (count fit-buckets))
+      (let [fewest-buckets-size (count (apply min-key count fit-buckets))
+            fewest-bucket-combos (filter #(= fewest-buckets-size (count %)) fit-buckets)]
+        (println "smalles subsets count" (count fewest-bucket-combos))))))
