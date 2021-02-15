3f666135c9d16f6ec24f939164f271027264f6c8
Anatolij Grigorjev <grigorjev.anatolij@gmail.com>
2021-02-15 08:58:22 +0200

    read input day17

--- 2015/day17/src/day17/main.clj
+++ 2015/day17/src/day17/main.clj
@@ -1,5 +1,10 @@
 (ns day17.main
   (:require [aoc-commons.core :as aoc]))
 
+(defn parse-bucket [line]
+  ((comp first first) (re-seq #"(\d+)" line)))
+
 (defn -main [& args]
-  (println "Initializing day17"))
+  (let [path (first args)
+        buckets (aoc/read-input-lines parse-bucket path)]
+    (println buckets)))
