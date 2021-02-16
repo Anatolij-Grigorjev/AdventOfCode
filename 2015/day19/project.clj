aaa0e285741fd9fb134cc762ac9c104dd4c13171
Anatolij Grigorjev <grigorjev.anatolij@gmail.com>
2021-02-16 11:07:16 +0200

    add empty day19

---
+++ 2015/day19/project.clj
@@ -0,0 +1,4 @@
+(defproject day19 "HEAD"
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.tiem625/aoc-commons "0.01"]]
+  :main day19.main)
---
+++ 2015/day19/src/day19/main.clj
@@ -0,0 +1,5 @@
+(ns day19.main
+  (:require [aoc-commons.core :as aoc]))
+
+(defn -main [& args]
+  (println "Initializing day19"))
