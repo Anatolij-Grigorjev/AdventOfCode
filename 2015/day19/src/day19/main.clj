d8aafdbf85634586e5a6c88f2d4edfe7a295c4cc
Anatolij Grigorjev <grigorjev.anatolij@gmail.com>
2021-02-17 14:53:28 +0200

    day19 parse input

--- 2015/day19/src/day19/main.clj
+++ 2015/day19/src/day19/main.clj
@@ -1,5 +1,20 @@
 (ns day19.main
-  (:require [aoc-commons.core :as aoc]))
+  (:require [aoc-commons.core :as aoc])
+  (:require [clojure.string :as str]))
+
+
+(defn parse-inputs [data-string]
+  (let [all-lines (str/split data-string #"\n")
+        input-molecule (last all-lines)
+        replacements (for [replacement all-lines
+                           :while (not (str/blank? replacement))
+                           :let [[[_ from to]] (re-seq #"(\w+) => (\w+)" replacement)]]
+                       [from to])]
+    {:replacements replacements
+     :input-molecule input-molecule}))
+
 
 (defn -main [& args]
-  (println "Initializing day19"))
+  (let [path (first args)
+        inputs (parse-inputs (slurp path))]
+    (println inputs)))
